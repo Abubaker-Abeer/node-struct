911619e1a643d750750d607affbd3355483290a9
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-02-19 03:44:37 +0200

    database connection string

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize('node10', 'root', '', {
-  host: 'localhost',
+export const sequelize = new Sequelize('freedb_node10', 'freedb_abeer@2001', 'C%u?BQnwbfZ8Hs?', {
+  host: 'sql.freedb.tech',
+  port:3306,
   dialect: 'mysql',
 });
 
